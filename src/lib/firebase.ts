fb04e42762738e35c085e87bfc462ce61e7e2f16
Withered Flowers <withered-flowers@users.noreply.github.com>
2026-01-30 09:33:41 +0700

    feat(data): Export Firebase app and Firestore instance

---
+++ src/lib/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDYlPztG3XmOvfMwaLs8U32elDNWjOnY44",
+	authDomain: "tainted-development.firebaseapp.com",
+	projectId: "tainted-development",
+	storageBucket: "tainted-development.firebasestorage.app",
+	messagingSenderId: "672379756589",
+	appId: "1:672379756589:web:92d088ebb496d00a6058a4",
+	measurementId: "G-ZZEZNKHV3Y",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
