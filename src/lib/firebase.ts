5c6c03e7c479a159aed712e706649c36c44be40a
Withered Flowers <withered-flowers@users.noreply.github.com>
2026-02-12 08:09:48 +0700

    feat(manual): Secure firebase, delete old api

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDYlPztG3XmOvfMwaLs8U32elDNWjOnY44",
-	authDomain: "tainted-development.firebaseapp.com",
-	projectId: "tainted-development",
-	storageBucket: "tainted-development.firebasestorage.app",
-	messagingSenderId: "672379756589",
-	appId: "1:672379756589:web:92d088ebb496d00a6058a4",
-	measurementId: "G-ZZEZNKHV3Y",
+	apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
+	authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.PUBLIC_FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
